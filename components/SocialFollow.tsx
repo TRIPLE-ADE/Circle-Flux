1eaadd064f0206def2e0682ed16185504133c721
Abdulrasheed <adetomiwaabdul@gmail.com>
2026-06-21 10:14:39 +0100

    chore: comment out X and LinkedIn social links in SocialFollow component

--- components/SocialFollow.tsx
+++ components/SocialFollow.tsx
@@ -15,28 +15,28 @@ const ICONS = [
     url: "https://www.facebook.com/share/1BKybNtFny/",
     icon: <path d="M14 8h-2a2 2 0 0 0-2 2v2H8v3h2v6h3v-6h2.5l.5-3h-3v-1.5c0-.8.3-1.2 1.2-1.2H16V8z" />,
   },
-  {
-    label: "X",
-    url: "#",
-    icon: (
-      <>
-        <path d="M6 6l12 12" />
-        <path d="M18 6L6 18" />
-      </>
-    ),
-  },
-  {
-    label: "LinkedIn",
-    url: "#",
-    icon: (
-      <>
-        <path d="M7 10v8" />
-        <path d="M7 7.2v.01" />
-        <path d="M11 18v-4.4c0-2.4 4-2.6 4 0V18" />
-        <path d="M11 10v8" />
-      </>
-    ),
-  },
+  // {
+  //   label: "X",
+  //   url: "#",
+  //   icon: (
+  //     <>
+  //       <path d="M6 6l12 12" />
+  //       <path d="M18 6L6 18" />
+  //     </>
+  //   ),
+  // },
+  // {
+  //   label: "LinkedIn",
+  //   url: "#",
+  //   icon: (
+  //     <>
+  //       <path d="M7 10v8" />
+  //       <path d="M7 7.2v.01" />
+  //       <path d="M11 18v-4.4c0-2.4 4-2.6 4 0V18" />
+  //       <path d="M11 10v8" />
+  //     </>
+  //   ),
+  // },
 ];
 
 export default function SocialFollow() {
